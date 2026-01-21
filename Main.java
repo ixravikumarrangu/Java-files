b71bb8753fd95c6337246ae37d12dfe6f6c0e3be
ixravikumarrangu <ravikumar.rangu@innovatechs.com>
2026-01-21 17:04:40 +0530

    Adding method implementation Java program

---
+++ Main.java
@@ -0,0 +1,11 @@
+public class Main{
+    public static void firstname(String name){
+        System.out.println(name+" Rangu");
+    }
+    public static void main(String[] args){
+        firstname("Ravi Kumar");
+        firstname("Kalyani");
+        firstname("Laxmana chary");
+        firstname("Manoj Kumar");
+    }
+}
