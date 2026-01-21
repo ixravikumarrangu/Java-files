6678e028b67f4bbac87268b6ecd13542dc08aea6
ixravikumarrangu <ravikumar.rangu@innovatechs.com>
2026-01-21 11:28:13 +0530

    Adding java files to the repo Java-files repo

---
+++ datatypes_ex.java
@@ -0,0 +1,11 @@
+class datatypes_ex{
+	public static void main(String[] args){
+		//boolean msm=false;
+		int var2=10;
+		float var3=var2;
+		int var4=(int)var3;
+		System.out.println(var2);
+		System.out.println(var3);
+		System.out.println(var4);
+	}
+}
---
+++ static_example.java
@@ -0,0 +1,8 @@
+class demo{
+	static int points=10;
+}
+class static_example{
+	public static void main(String[] args){
+		System.out.println(demo.points);
+	}
+}
